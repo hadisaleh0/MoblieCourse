50bba9a70a683b4ff3ac77ef48a6d088fbee3489
hadisaleh0 <hadysaleh313@gmail.com>
2024-02-20 13:38:49 +0200

    Second Version

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.mobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button displayButton;
+
+    EditText nameEditText;
+
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initlayoutComponents();
+        initButtonDisplay();
     }
+
+    private void initlayoutComponents(){
+
+        displayButton = findViewById(R.id.Button);
+        nameEditText  = findViewById(R.id.editTextName);
+        displayText   = findViewById(R.id.textViewDisplay);
+
+    }
+
+    private void initButtonDisplay(){
+        displayButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String nametoDisplay = nameEditText.getText().toString();
+                displayText.setText("Hello "+ nametoDisplay);
+            }
+        });
+
+    }
+
 }
